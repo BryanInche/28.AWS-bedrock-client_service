15cbed41780a46dad7a2ae1c584bbcbd6d75782e
BryanInche <brayaninchecondor@gmail.com>
2026-06-25 14:49:07 -0500

    avance_1_aws_boto3_dev

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
 import pandas as pd
+import boto3
+import streamlit as st
+import json
+import requests
+import os
+import base64
+
+region = "us-east-1"
+os.environ["AWS_REGION"] = region
+llm_response = ""
+
+aws = boto3.session.Session(profile_name="genaiday", region_name=region)
+client = aws.client("bedrock-runtime")
