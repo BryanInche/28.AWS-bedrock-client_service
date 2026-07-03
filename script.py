96bc0f9bf0a71e1cdd11ee8510948882b2663b12
BryanInche <brayaninchecondor@gmail.com>
2026-07-03 14:49:04 -0500

    code_imagen_deteccion-multimodal

--- script.py
+++ script.py
@@ -7,11 +7,19 @@ import os
 import base64
 import mimetypes
 
+# 1. El Entorno Virtual Aislado (venv_aws)
+
 region = "us-east-1"
 os.environ["AWS_REGION"] = region
 llm_response = ""
 
-# 1. Creamos una "Sesión" de AWS.
+# 2. El Repositorio de Código Git Vinculado (main branch en GitHub)
+
+
+# 3. El Perfil de Autenticación Local de AWS CLI (profile_name="genaiday")
+# Almacena de forma segura tus llaves de acceso (Access Key y Secret Key) en el sistema operativo Windows y las asocia a un perfil específico con 
+# permisos controlados para interactuar con Amazon Bedrock.
+# Creamos una "Sesión" de AWS.
 # Esto lee el archivo oculto en tu computadora donde guardamos tus llaves secretas
 # al ejecutar 'aws configure --profile genaiday'. Así, AWS sabe que eres tú
 # y que tienes permisos, sin tener que escribir contraseñas en el código.
@@ -26,6 +34,11 @@ client = aws.client("bedrock-runtime")  # Uso de cliente bedrock-runtime
 # administrar modelos, crear custom models y fine-tuning) y 
 # bedrock-runtime (diseñado exclusivamente para la inferencia de baja 
 # latencia en tiempo real).
+
+
+#4. El Mapeador Multimodal (read_mime_type)
+#Analiza dinámicamente cualquier extensión de archivo (incluyendo formatos web optimizados como .webp) 
+# y extrae su identificador estándar de internet (MIME Type).
 
 ##################################################
 #Preparación Multimodal
@@ -95,10 +108,53 @@ def call_text(prompt, modelId="anthropic.claude-3-haiku-20240307-v1:0"):
 
 
 
+
+def call_multimodal(file,caption,modelId="anthropic.claude-3-haiku-20240307-v1:0"):
+    #esta funcion es para llamar a un modelo multimodal con una imagen y un texto
+    config = {
+    "anthropic_version": "bedrock-2023-05-31",
+    "max_tokens": 4096,
+    "messages": [
+        {
+            "role": "user",
+            "content": [
+                {
+                     "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": read_mime_type(file),
+                            "data": base64.b64encode(open(file, "rb").read()).decode("utf-8")
+                        }
+                },
+                {
+                    "type": "text",
+                    "text": caption
+            }]
+        }
+    ]
+    }
+
+    body = json.dumps(config)
+    modelId = modelId
+    accept = "application/json"
+    contentType = "application/json"
+
+    response = client.invoke_model(
+    body=body, modelId=modelId, accept=accept, contentType=contentType)
+    response_body = json.loads(response.get("body").read())
+    results = response_body.get("content")[0].get("text")
+    return results
+
+
+
+##################################################
+# Validar conexion al modelo de AWS bedrock
+#################################################
+# 1. Procesamiento de Lenguaje Natural como input para el LLM
 prompt = "Estoy por abrir una cafeteria al paso, recomiendame 5 nombres"
-call_text(prompt, "anthropic.claude-3-haiku-20240307-v1:0")
+call_text(prompt, "anthropic.claude-3-haiku-20240307-v1:0")  # Si se quiere se puede cambiar el modelo (modificando el Model_id, que se tienen listado en AWS Bedrock)
 
-
-
-
-
+# 2. Procesamiento de Imagen como input para el LLM
+pic_path = "./meetup_test_image.jpg"
+caption = "Cuantas personas hay en la imagen? cuantas laptos ves? cuantos usan gorro o sombrero?, de que color es el hoddie de la primer persona a la derecha de la foto?"
+print(call_image(pic_path,caption,"anthropic.claude-3-haiku-20240307-v1:0"))
